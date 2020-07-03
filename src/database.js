bf4e7482df614fdd524919572cf0c8d528432502
Daniela <danielapv200@gmail.com>
2020-07-02 21:32:50 -0500

    variables

--- src/database.js
+++ src/database.js
@@ -1,22 +1,12 @@
 const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
-  host: process.env.BD_HOST || "localhost" ,
-  user: process.env.BD_USER || "root",
-  password: process.env.BD_PASSWORD || "",
-  database: process.env.BD_DATABASE || "bdvehiculos",
+  host: process.env.MYSQL_ADDON_HOST || "localhost" ,
+  user: process.env.MYSQL_ADDON_USER || "root",
+  password: process.env.MYSQL_ADDON_PASSWORD || "",
+  database: process.env.MYSQL_ADDON_DB || "bdvehiculos",
   multipleStatements: true
 });
-
-/*
-const mysqlConnection = mysql.createConnection({
-  host: 'bbyoozip4dqamyfg4ehp-mysql.services.clever-cloud.com',
-  user: 'u4stqwk7jvaaldbg',
-  password: 'l6joEfd1cMrK9KQEGwcN',
-  database: 'bbyoozip4dqamyfg4ehp',
-  multipleStatements: true
-});
-*/
 
 mysqlConnection.connect(function (err) {
   if (err) {
