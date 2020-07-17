a94a48ffd9e63f44f08fb9c802e3dc2bd772feba
Daniela <danielapv200@gmail.com>
2020-07-17 00:48:11 -0500

    distritos

--- src/routes/distritos.js
+++ src/routes/distritos.js
@@ -4,7 +4,17 @@ const router = express.Router();
 const mysqlConnection  = require('../database.js');
 
 router.get('/', (req, res) => {
-  mysqlConnection.query('SELECT idDistrito value, provincia, distrito label, costo FROM distrito', (err, rows, fields) => {
+  mysqlConnection.query('SELECT idDistrito value, provincia, distrito label, costo FROM distrito where estado=1', (err, rows, fields) => {
+    if(!err) {
+      res.json(rows);
+    } else {
+      console.log(err);
+    }
+  });  
+}); 
+
+router.get('/all', (req, res) => {
+  mysqlConnection.query('SELECT idDistrito value, provincia, distrito label, costo, estado FROM distrito', (err, rows, fields) => {
     if(!err) {
       res.json(rows);
     } else {
